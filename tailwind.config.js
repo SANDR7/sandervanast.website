d87679275610dc7dbad706644b3f0366069b2280
Sander <Sandervanast1@gmail.com>
2022-02-14 22:43:01 +0100

    more config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  darkMode: 'class',
     content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
