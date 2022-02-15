1eff694bfc7ced4cd9d7c0fba156902f537b8fd1
Sander <Sandervanast1@gmail.com>
2022-02-15 22:23:36 +0100

    adding github endpoint

---
+++ pages/api/github.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { repoProps } from '../../types/repos';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const userResponse = await fetch('https://api.github.com/users/SANDR7', {
+        headers: {
+            "Authorization": `Token ${process.env.GITHUB_FETCH_KEY}`
+        }
+    });
+    const userReposResponse = await fetch(
+        'https://api.github.com/users/SANDR7/repos?per_page=3&sort=pushed', {
+        headers: {
+            "Authorization": `Token ${process.env.GITHUB_FETCH_KEY}`
+        }
+    }
+    );
+
+    const user = await userResponse.json();
+    const repositories = await userReposResponse.json();
+    const latest_repos: any[] = [];
+    repositories.map((repo: repoProps) => latest_repos.push({
+        name: repo.name,
+        full_name: repo.full_name,
+        language: repo.language,
+        description: repo.description,
+        repo_url: repo.html_url,
+        topics: repo.topics,
+        stars: repo.stargazers_count,
+    }));
+
+    res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=1200, stale-while-revalidate=600'
+    );
+
+    return res.status(200).json({
+        username: user.login,
+        follower: user.followers,
+        public_repos: user.public_repos,
+        latest_repos,
+    });
+}
---
+++ types/repos.ts
@@ -0,0 +1,10 @@
+export interface repoProps {
+    name: string;
+    full_name: string;
+    language: string;
+    description: string;
+    repo_url: string;
+    html_url: string;
+    stargazers_count: number;
+    topics: [];
+}
