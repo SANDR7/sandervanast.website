4dbcc1a7e4ec4f0c96675da9d53e18129306bb18
Sander van Ast <sandervanast1@gmail.com>
2022-02-15 11:36:47 +0100

    tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   darkMode: 'class',
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layout/**/*.{js,ts,jsx,tsx}",
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    colors: {
+      blue: '#4763FF',
+      red: '#C94040',
+      orange: '#FF7B47',
+      green: '#38C4B2',
+      'gray-dark': '#2E2E2E',
+      'gray-light': '#F9F9F9'
+    },
+    fontFamily: {
+      sans: ['rubik', 'sans-serif']
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
