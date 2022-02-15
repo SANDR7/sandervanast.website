c9de7d6d75178739370143ca09bcd7e421ecf235
Sander van Ast <sandervanast1@gmail.com>
2022-02-15 17:06:37 +0100

    fixing head component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html lang="en">
+      <Head>
+        {/* <link href="/static/favicons/favicon.ico" rel="shortcut icon" /> */}
+        {/* <link href="/static/favicons/site.webmanifest" rel="manifest" /> */}
+        <meta content="#F9F9F9" name="theme-color" />
+        <meta content="#F9F9F9" name="msapplication-TileColor" />
+      </Head>
       <body className="transition-colors bg-gray-light dark:bg-gray-dark  text-gray-dark dark:text-gray-light">
         <Main />
         <NextScript />
