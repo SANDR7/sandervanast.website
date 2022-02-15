6ee172f7028be49a6c4827fb389d7cc70ac2d127
Sander <Sandervanast1@gmail.com>
2022-02-15 21:14:39 +0100

    adding fontawesome icons

--- layout/Row.tsx
+++ layout/Row.tsx
@@ -8,19 +8,28 @@ interface RowProps {
     url: string;
     name: string;
   };
-  linkNewTab: boolean;
+  linkNewTab?: boolean;
   children: ReactNode;
 }
 
-const Row: FC<RowProps> = ({ title, description, link, children, linkNewTab = false }) => {
+const Row: FC<RowProps> = ({
+  title,
+  description,
+  link,
+  children,
+  linkNewTab = false
+}) => {
   return (
-    <div className=''>
+    <div className="">
       <h2 className="header">{title}...</h2>
-      {description && <p className='text-gray-mid'>{description}</p>}
+      {description && <p className="text-gray-mid">{description}</p>}
       {link && (
-        <Link href={link.url} passHref>
-          <a target={linkNewTab ? '_blank' : '_self'}>{link.name}</a>
-        </Link>
+        <span className="group ">
+          <Link href={link.url} passHref>
+            <a target={linkNewTab ? '_blank' : '_self'}>{link.name}</a>
+          </Link>
+          <i className="fa-solid fa-arrow-right ml-1 transition-transform transform translate-x-1 group-hover:translate-x-3"></i>
+        </span>
       )}
       <div>{children}</div>
     </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,11 @@ const Document = () => {
         {/* <link href="/static/favicons/site.webmanifest" rel="manifest" /> */}
         <meta content="#F9F9F9" name="theme-color" />
         <meta content="#F9F9F9" name="msapplication-TileColor" />
+        <script
+          async
+          src="https://kit.fontawesome.com/e48f77a7cf.js"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body className="transition-colors bg-gray-light dark:bg-gray-dark  text-gray-dark dark:text-gray-light">
         <Main />
