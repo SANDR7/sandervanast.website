6fe2ed627cde60d160ca325112b2f762e9a72def
Sander <Sandervanast1@gmail.com>
2022-02-16 22:23:08 +0100

    adding navigation

--- layout/Main.tsx
+++ layout/Main.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import { useTheme } from 'next-themes';
-import React, { FC, ReactNode, useEffect, useState } from 'react';
+import React, { FC, ReactNode } from 'react';
+import Navigation from './Navigation';
 
 interface MainProps {
   children: ReactNode;
@@ -9,11 +9,6 @@ interface MainProps {
 }
 
 const PageContainer: FC<MainProps> = ({ children, ...customMeta }) => {
-  const [mounted, setMounted] = useState(false);
-  const { resolvedTheme, setTheme } = useTheme();
-
-  // After mounting, we have access to the theme
-  useEffect(() => setMounted(true), []);
 
   const meta = {
     title: 'Sander van Ast — Developer & Designer',
@@ -42,13 +37,11 @@ const PageContainer: FC<MainProps> = ({ children, ...customMeta }) => {
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
       </Head>
-      <button
-        aria-label="Toggle Dark Mode"
-        type="button"
-        onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-      >
-        {resolvedTheme === 'dark' ? <span>light</span> : <span>dark</span>}
-      </button>
+
+<div className='Container'>
+      <Navigation/>
+</div>
+    
 
       <main className='Container'>{children}</main>
     </>
