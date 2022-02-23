04e37055d8f0e24569543278f2b5b3661947be71
Sander van Ast <sandervanast1@gmail.com>
2022-02-23 16:21:23 +0100

    changing copy

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,32 +1,33 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-    const services = [
-        {
-            text: 'Development',
-            explanation: 'some text'
-        },
-        {
-            text: 'Graphic Design',
-            explanation: "I offer graphic designs. That can be logos, illustrations or layouts. I do this besides my main offering, web development. If you choose layout? that means a wireframe with a optional prototype."
-        },
-        {
-            text: 'Technology',
-            explanation: "A web app is a  websites that can be download to a desktop and/or smartphones and is in my opinion the future of web development. The website is than almost transform into a native app how you see them in the app store."
-        },
-        {
-            text: 'Nerd Talk ... ( Tech, Keyboards, Cubes ) ',
-            explanation: ""
-        },
-    ]
+  const services = [
+    {
+      text: 'Development',
+      explanation:
+        'We are all human. Sharing is in our nature, so why not write about it...'
+    },
+    {
+      text: 'Graphic Design',
+      explanation:
+        'I offer graphic designs. That can be logos, illustrations or layouts. I do this besides my main offering, web development. If you choose layout? that means a wireframe with a optional prototype.'
+    },
+    {
+      text: 'Technology',
+      explanation:
+        'A web app is a  websites that can be download to a desktop and/or smartphones and is in my opinion the future of web development. The website is than almost transform into a native app how you see them in the app store.'
+    },
+    {
+      text: 'Nerd Talk... ',
+      explanation:
+        "Everything tech, keyboards, and design. I'm also open for things like cubes, lego, music"
+    }
+  ];
 
-
-
-
-    res.send({
-        services,
-        length: services.length
-    })
-}
+  res.send({
+    services,
+    length: services.length
+  });
+};
 
 export default handler;
