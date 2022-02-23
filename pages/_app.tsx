5d5fa0eabcc59e1f0988f4483b7b7262317ddaab
Sander van Ast <sandervanast1@gmail.com>
2022-02-23 11:58:46 +0100

    Fixing package modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Script src="https://kit.fontawesome.com/e48f77a7cf.js" strategy='afterInteractive'></Script>
+      <Script src="https://kit.fontawesome.com/e48f77a7cf.js" strategy='afterInteractive'/>
       <Component {...pageProps} />
     </ThemeProvider>
   );
